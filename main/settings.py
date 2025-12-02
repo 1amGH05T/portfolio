c28693b7453c1dde9c9c8d599f7c7526ca1141c3
GHOST100919 <ladejebijoshua@gmail.com>
2025-12-02 11:21:30 +0100

    allowedhost

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!6clwxpi7bf3c(6xx#zzqmie@$fem*4)c044n8&exl8g2j4gus
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oneamgh05t.onrender.com']
 
 
 # Application definition
